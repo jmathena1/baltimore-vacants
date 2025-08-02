14b5e998813f49784e2022b59e2f64daf9077be7
John Mathena <bobtart41@gmail.com>
2025-08-02 10:32:37 -0400

    small updates and more dependencies

--- bmore-vacants.py
+++ bmore-vacants.py
@@ -12,11 +12,21 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     There are about 12,500 vacant homes in Baltimore City, according to the Mayor's office. 
 
-    City, state, and federal entities own about 1,000 of those houses. So who owns the rest?
+    City, state, and federal entities own about 1,000 of those houses. 
+
+    So who owns the rest? 
+    """).callout(kind='neutral')
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+
 
     Thanks to Open Baltimore data, we can filter the [Vacant Building Notices](https://data.baltimorecity.gov/datasets/691d65a5f85640e6aaa46930bd9dc102_1/explore?location=39.296383%2C-76.620458%2C11.51&showTable=true) (VBNs) dataset with the city's [Real Property Information](https://data.baltimorecity.gov/datasets/baltimore::real-property-information-2/explore?location=39.294088%2C-76.628072%2C14.18&showTable=true) to gather mailing addresses on file for all vacant homes.
     """
@@ -54,11 +64,26 @@ def _(mo, real_property_data, vacant_houses):
             v.*,
             r.*
         from vacant_houses as v
-        join real_property_data as r on v."BLOCKLOT" = r."BLOCKLOT"
+        left join real_property_data as r on v."BLOCKLOT" = r."BLOCKLOT"
         """,
         output=False
     )
     return (vacant_houses_with_owners,)
+
+
+@app.cell
+def _(mo, vacant_houses_with_owners):
+    _df = mo.sql(
+        f"""
+        select
+        "BLOCKLOT",
+        count("BLOCKLOT") as real_property_count
+        from vacant_houses_with_owners
+        group by "BLOCKLOT"
+        order by real_property_count desc
+        """
+    )
+    return
 
 
 @app.cell
@@ -72,6 +97,8 @@ def _(mo):
     The real property data is very wide (~200 columns, I think?), so I only kept the mailing address and block lot columns; the block lot is shared between datasets and allows me to connect a vacant home to an owner (or at least a mailbox where someone can reach the owner).
 
     In the above 3 cells, I read the two CSVs into DuckDB tables and join them on the `BLOCKLOT` column.
+
+    I noticed that there were more records on my resulting dataset after the join. So I ran a query to count how many times each block lot appeared in the combined dataset with vacant houses and addresses. I counted 7 block lots that appear more than once in the real property records. My guess is that these properties have apartments, are mixed use, or have ADUs on the block lot. 
     """
     )
     return
@@ -167,23 +194,28 @@ def _(bmore_vacant_owners, vacant_houses_with_owners):
 
 
 @app.cell
-def _(bmore_vacant_owners_count, local_ownership_rate, mo):
+def _(mo):
     mo.md(
         rf"""
     ## Analysis
 
     I grabbed Baltimore area zip codes from [Zip Codes.com](https://www.zip-codes.com/city/md-baltimore.asp). Now this will technically pull some county addresses, but I'm just trying to see who may own these vacants and is still in the area so I'm good with that for now.
 
-    Next, I extracted the zip code from the mailing address listed for each vacant home by pulling the last five values in each address string. I eyeballed the data and it looked like that was always the zip code.
+    Next, I extracted the zip code from the mailing address listed for each vacant home by pulling the last five values in each address string. I eyeballed the data and it looked the last five values was always the zip code.
 
     Finally, I checked if the mailing address zip code lived in our Baltimore area zip code list and attempted to filter out PO Boxes with a regular expression.
+    """
+    )
+    return
 
+
+@app.cell
+def _(bmore_vacant_owners_count, local_ownership_rate, mo):
+    mo.md(f"""
     **So by my count, {bmore_vacant_owners_count} vacant houses in Baltimore are owned by an entity with a mailing address nearby.**
 
     **That's {local_ownership_rate}% of vacant houses citywide.**
-
-    """
-    )
+    """).callout(kind='info')
     return
 
 
@@ -199,7 +231,11 @@ def _(mo):
     But, it's almost worse that most the people sitting on thoses houses *are* from 'round here! Can y'all help us out??
 
     ### How accurate are these mailing addresses?
-    I do not know how the city adds these addresses to the real property records. It is possible that out of town developers are using someone local to hide the fact that they are doing nothing with houses that could use some love. I know for a fact that when homeowners die, it can take the city and state a *very* long to update the owner and point of contact for a property if it is no longet, owner occupied. But, vacants may have a more stringent process and I did try to eliminate PO Boxes.
+    I do not know how the city adds these addresses to the real property records. It is possible that out of town developers are using someone local to hide the fact that they are doing nothing with houses that could use some love. 
+
+    I know for a fact that when homeowners die, it can take the city and state a *very* long to update the owner and point of contact for a property if it is no longer owner occupied. 
+
+    But, vacants may have a more stringent process and I did try to eliminate PO Boxes.
     """
     )
     return
